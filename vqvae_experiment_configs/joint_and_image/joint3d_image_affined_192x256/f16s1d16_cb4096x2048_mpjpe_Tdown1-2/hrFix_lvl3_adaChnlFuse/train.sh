854fabef0e06873f394f98b02c6ccbb6f982e591
BradleyWang0416 <993369949@qq.com>
2025-10-06 10:05:38 +0000

    exps done or stopped

--- vqvae_experiment_configs/joint_and_image/joint3d_image_affined_192x256/f16s1d16_cb4096x2048_mpjpe_Tdown1-2/hrFix_lvl3_adaChnlFuse/train.sh
+++ vqvae_experiment_configs/joint_and_image/joint3d_image_affined_192x256/f16s1d16_cb4096x2048_mpjpe_Tdown1-2/hrFix_lvl3_adaChnlFuse/train.sh
@@ -2,19 +2,18 @@
 # mode=train
 mode=test
 
-EXP_NAME=joint_only/joint3d_image_affined_192x256/f64s1d64_cb4096x2048_mpjpe
-CONFIG=vqvae_experiment_configs/joint_only/joint3d_image_affined_192x256/f64s1d64_cb4096x2048_mpjpe/config.yaml
-LOG=vqvae_experiment_configs/joint_only/joint3d_image_affined_192x256/f64s1d64_cb4096x2048_mpjpe/train.log
-
+EXP_NAME="joint_and_image/joint3d_image_affined_192x256/f16s1d16_cb4096x2048_mpjpe_Tdown1-2/hrFix_lvl3_adaChnlFuse"
+CONFIG="vqvae_experiment_configs/joint_and_image/joint3d_image_affined_192x256/f16s1d16_cb4096x2048_mpjpe_Tdown1-2/hrFix_lvl3_adaChnlFuse/config.yaml"
+LOG="vqvae_experiment_configs/joint_and_image/joint3d_image_affined_192x256/f16s1d16_cb4096x2048_mpjpe_Tdown1-2/hrFix_lvl3_adaChnlFuse/train.log"
 
 if [ "$mode" = "test" ]; then
-    RESUME_PATH="vqvae_experiment/joint_only/joint3d_image_affined_192x256/f64s1d64_cb4096x2048_mpjpe/models/checkpoint_epoch_399_step_300000"
+    RESUME_PATH="vqvae_experiment/joint_and_image/joint3d_image_affined_192x256/f16s1d16_cb4096x2048_mpjpe_Tdown1-2/hrFix_lvl3_adaChnlFuse/models/checkpoint_epoch_237_step_180000"
     LOSS_TYPE=mpjpe_millimeter     # l1, mpjpe
     BATCH_SIZE=64
 else
     RESUME_PATH=""
     LOSS_TYPE=mpjpe     # l1, mpjpe
-    BATCH_SIZE=32
+    BATCH_SIZE=64
 fi
 
 
@@ -22,14 +21,14 @@ DATA_MODE=joint3d
 
 NUM_CODE=4096   # 8192
 CODE_DIM=2048   # 3072
-NUM_FRAME=64
+NUM_FRAME=16
 SAMPLE_STRIDE=1
-DATA_STRIDE=64
+DATA_STRIDE=16
 
 
 VQVAE_TYPE=hybrid  # base, hybrid
 HRNET_OUTPUT_LEVEL=3    # int or list. 0,1,2,3 分别对应输出 [B,32,H/4,W/4], [B,64,H/8,W/8], [B,128,H/16,W/16], [B,256,H/32,W/32] 的特征
-VISION_GUIDANCE_RATIO=0
+VISION_GUIDANCE_RATIO=-1
 
 FIX_WEIGHTS=True
 
@@ -41,7 +40,7 @@ else
 fi
 
 if [ "$mode" = "debug" ]; then
-    CUDA_VISIBLE_DEVICES=5 \
+    CUDA_VISIBLE_DEVICES=4 \
         python \
         -m debugpy --listen 5680 --wait-for-client \
         train_vqvae_new.py \
@@ -59,11 +58,14 @@ if [ "$mode" = "debug" ]; then
         --vqvae_type ${VQVAE_TYPE} \
         --hrnet_output_level ${HRNET_OUTPUT_LEVEL} \
         --vision_guidance_ratio ${VISION_GUIDANCE_RATIO} \
+        --downsample_time "[1,2]" \
+        --frame_upsample_rate "[2.0,1.0]" \
         $FIX_WEIGHTS_ARG \
         --resume_pth "${RESUME_PATH}"
-        # -m debugpy --listen 5680 --wait-for-client \
 elif [ "$mode" = "test" ]; then
-    CUDA_VISIBLE_DEVICES=1 \
+        # -m debugpy --listen 5680 --wait-for-client \
+        # accelerate launch --num_processes 5 \
+    CUDA_VISIBLE_DEVICES=3 \
         python \
         test_vqvae_new.py \
         --config ${CONFIG} \
@@ -78,11 +80,15 @@ elif [ "$mode" = "test" ]; then
         --data_stride ${DATA_STRIDE} \
         --data_mode ${DATA_MODE} \
         --hrnet_output_level ${HRNET_OUTPUT_LEVEL} \
-        --vision_guidance_ratio ${VISION_GUIDANCE_RATIO}
+        --vision_guidance_ratio ${VISION_GUIDANCE_RATIO} \
+        --downsample_time "[1,2]" \
+        --frame_upsample_rate "[2.0,1.0]"
 else
-    CUDA_VISIBLE_DEVICES=2 \
+        # python -u train_vqvae_new.py \
+    CUDA_VISIBLE_DEVICES=6,7 \
         nohup \
-        python -u train_vqvae_new.py \
+        accelerate launch --num_processes 2 --main_process_port 29017 \
+        train_vqvae_new.py \
         --batch_size ${BATCH_SIZE} \
         --config ${CONFIG} \
         --data_mode ${DATA_MODE} \
@@ -97,6 +103,8 @@ else
         --vqvae_type ${VQVAE_TYPE} \
         --hrnet_output_level ${HRNET_OUTPUT_LEVEL} \
         --vision_guidance_ratio ${VISION_GUIDANCE_RATIO} \
+        --downsample_time "[1,2]" \
+        --frame_upsample_rate "[2.0,1.0]" \
         $FIX_WEIGHTS_ARG \
         --resume_pth "${RESUME_PATH}" \
         > ${LOG} &
